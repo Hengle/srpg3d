686dc3dde2e76aa41d8e88e5651990bddd870102
Tony Sun <quansa.sun@gmail.com>
2016-09-15 02:53:24 +1200

    Board editor scene

---
+++ Assets/Scripts/Model/Point.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct Point {
+	public int x, y;
+
+	public Point(int _x, int _y) {
+		x=_x;
+		y=_y;
+	}
+
+	public static Point operator +(Point a, Point b) {
+		return new Point(a.x + b.x, a.y + b.y);
+	}
+
+	public static Point operator -(Point p1, Point p2) {
+		return new Point(p1.x - p2.x, p1.y - p2.y);
+	}
+
+	public static bool operator ==(Point a, Point b) {
+		return a.x == b.x && a.y == b.y;
+	}
+
+	public static bool operator !=(Point a, Point b) {
+		return !(a == b);
+	}
+
+	public override bool Equals(object obj) {
+		return (obj is Point) ? this == (Point)obj : false;
+	}
+
+	public override int GetHashCode() {
+		return ((x+13) * 7) + y;
+	}
+	
+	public override string ToString() {
+		return string.Format("({0},{1})", x, y);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/View Model Component/Tile.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public const float stepHeight = 0.25f;
+	public Point pos;
+	public int height;
+
+	public Vector3 center { get { return new Vector3(pos.x, height * stepHeight, pos.y); } }
+
+	void Match() {
+		transform.localPosition = new Vector3(pos.x, height * stepHeight / 2f, pos.y); // places centre of tile here
+		transform.localScale = new Vector3(1, height * stepHeight, 1); // scales so bottom of tile at height 0ddd
+	}
+
+	public void Grow() {
+		height++;
+		Match();
+	}
+
+	public void Shrink() {
+		height--;
+		Match();
+	}
+
+	public void Load(Point p, int h) {
+		pos = p;
+		height = h;
+		Match();
+	}
+
+	public void Load(Vector3 v) {
+		Load(new Point((int)v.x, (int)v.z), (int)v.y); // <- rounding?
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
